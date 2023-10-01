0cc72a29cf711a60d9d5d9bd2c6953a2ce3e4501
Luis Almenarez <luisalmenarez0428@gmail.com>
2023-09-30 21:52:06 -0500

    Se agrega corrección al menú hamburguesa // Aún falta responsive del hero y sección products

--- public/JS/app.js
+++ public/JS/app.js
@@ -1,17 +1,25 @@
 // Obtén el botón de abrir/cerrar el menú
 const nav = document.querySelector(".header__nav");
 const open = document.querySelector(".open__menu");
+const main = document.querySelector(".main");
 
 // Agrega un controlador de eventos al botón
 open.addEventListener("click", () => {
   // Verifica si el menú está abierto o cerrado
   if (nav.classList.contains("visible")) {
     // Cierra el menú
-    nav.classList.remove("visible");
+    nav.classList.toggle("visible");
     nav.style.transform = "translateX(100%)"; // Mueve el menú fuera de la pantalla
   } else {
     // Abre el menú
     nav.classList.add("visible");
     nav.style.transform = "translateX(0)"; // Muestra el menú deslizándolo desde la izquierda
+  }
+});
+
+document.addEventListener("click", (e) => {
+  if (main.contains(e.target) && e.target !== open) {
+    nav.classList.remove("visible");
+    nav.style.transform = "translateX(100%)";
   }
 });
